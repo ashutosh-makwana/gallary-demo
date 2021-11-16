d44c13ec462ef34551f33a5e3938c34eb40d365c
Ashutosh Makwana <ashutosh.kadiya@gmail.com>
2021-11-16 23:36:44 +0530

    Add Album list view model

---
+++ app/src/main/java/com/fun/gallerydroid/presentation/ui/album/UserAlbumViewModel.kt
@@ -0,0 +1,53 @@
+package com.`fun`.gallerydroid.presentation.ui.album
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.`fun`.gallerydroid.common.Resource
+import com.`fun`.gallerydroid.data.remote.dto.AlbumDto
+import com.`fun`.gallerydroid.domain.usecase.GetAlbumsUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import javax.inject.Inject
+
+@HiltViewModel
+class UserAlbumViewModel @Inject constructor(
+    private val getAlbumsUseCase: GetAlbumsUseCase
+) : ViewModel() {
+
+    // Backing property to avoid state updates from other classes
+    private val _uiState: MutableLiveData<UserPostsUiState> =
+        MutableLiveData()
+
+    // The UI collects from this StateFlow to get its state updates
+    val uiState: LiveData<UserPostsUiState> = _uiState
+
+    // Represents different states for the user posts list screen
+    sealed class UserPostsUiState {
+        object Loading : UserPostsUiState()
+        data class Success(val posts: List<AlbumDto>) : UserPostsUiState()
+        data class Error(val exception: String) : UserPostsUiState()
+    }
+
+    private fun getPosts(id:Int) {
+        getAlbumsUseCase.getAlbumPhotos(id).onEach { result ->
+            when (result) {
+                is Resource.Success -> {
+                    result.data?.let {
+                        _uiState.value = UserPostsUiState.Success(it)
+                    }
+                }
+                is Resource.Error -> {
+                    _uiState.value =
+                        UserPostsUiState.Error(result.message ?: "An unexpected error occurred")
+                }
+                is Resource.Loading -> {
+                    _uiState.value = UserPostsUiState.Loading
+                }
+            }
+        }.launchIn(viewModelScope)
+    }
+
+}
