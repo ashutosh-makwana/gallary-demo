a09136aa3d42235854bfb8e0cee35a1a2409a2e0
Ashutosh Makwana <ashutosh.kadiya@gmail.com>
2021-11-16 22:12:29 +0530

    Add api-retrofit interface

---
+++ app/src/main/java/com/fun/gallerydroid/data/remote/UserAlbumsApi.kt
@@ -0,0 +1,20 @@
+package com.`fun`.gallerydroid.data.remote
+
+import com.`fun`.gallerydroid.data.remote.dto.AlbumData
+import com.`fun`.gallerydroid.data.remote.dto.PhotoData
+import com.`fun`.gallerydroid.data.remote.dto.UserData
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface UserAlbumsApi {
+
+    @GET("/users")
+    suspend fun getUsers(): UserData
+
+    @GET("/albums")
+    suspend fun getAlbumsByUser(@Query("userId") userId:String): AlbumData
+
+    @GET("/photos")
+    suspend fun getPhotosByAlbum(@Path("coinId") coinId: String): PhotoData
+}
