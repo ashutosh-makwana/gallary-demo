a9861f7c71a36921a737620426d763bfad2583b6
Ashutosh Makwana <ashutosh.kadiya@gmail.com>
2021-11-16 22:36:30 +0530

    Refactor classes & add hilt annotations

--- app/src/main/java/com/fun/gallerydroid/presentation/MainActivity.kt
+++ app/src/main/java/com/fun/gallerydroid/presentation/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.`fun`.gallerydroid
+package com.`fun`.gallerydroid.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.`fun`.gallerydroid.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
